74e2e6a9b07bce0888e6c66101d2a40c9abd0a18
bedirhan çakır <bedirhancakir@windowslive.com>
2023-10-10 15:24:46 +0300

    Npc vision added

--- Assets/Scripts/Npc.cs
+++ Assets/Scripts/Npc.cs
@@ -6,34 +6,42 @@ public class Npc : MonoBehaviour
 {
     public static event System.Action OnGuardHasSpottedPlayer;
     public float speed = 8;
-    public float waitTime = .2f;
     public float timeToSpotPlayer = .5f;
     public Light spotlight;
     public LayerMask viewMask;
     public float viewDistance;
     float viewAngle;
     Color originalSpotlightColor;
-    Transform player;
+    public Transform player;
     void Start()
     {
         viewAngle = spotlight.spotAngle;
         originalSpotlightColor = spotlight.color;
         player = GameObject.FindGameObjectWithTag("Player").transform;
+        originalSpotlightColor = spotlight.color;
     }
 
     void Update()
     {
-        
+        if (CanSeePlayer())
+        {
+            spotlight.color = Color.red;
+        }
+        else
+        {
+            spotlight.color = originalSpotlightColor;
+        }
+        print(CanSeePlayer());
     }
     bool CanSeePlayer()
     {
-        if(Vector2.Distance(transform.position, player.position)< viewDistance)
+        if(Vector2.Distance(transform.position, player.position) < viewDistance)
         {
-            Vector2 dirToPlayer = (player.position - transform.position).normalized;
-            float angleBetweenGuardAndPlayer = Vector2.Angle(transform.forward, dirToPlayer);
-            if(angleBetweenGuardAndPlayer < viewAngle / 2f)
+            Vector2 dirToPlayer = (player.position - transform.position);
+            float angleBetweenGuardAndPlayer = Vector2.Angle(transform.right, dirToPlayer);
+            if(angleBetweenGuardAndPlayer < viewAngle)
             {
-                if(!Physics2D.Linecast(transform.position, player.position, viewMask))
+                if(Physics2D.Linecast(transform.position, player.position, viewMask))
                 {
                     return true;
                 }
