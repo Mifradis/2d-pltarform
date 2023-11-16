9b0a223756dd96401e1753a58457aa8a652c9696
bedirhan çakır <bedirhancakir@windowslive.com>
2023-11-16 15:47:51 +0300

    attack combo added

--- Assets/Scripts/PlayerAttack.cs
+++ Assets/Scripts/PlayerAttack.cs
@@ -5,10 +5,12 @@ using UnityEngine;
 public class PlayerAttack : MonoBehaviour
 {
     bool isHitting = false;
-    float attackTime = 0.5f;
+    float attackTime = 0.4f;
     float nextAttackTime;
     public PlayerInput attackInp;
     public Animator animator;
+    float firstAttackTime = 0;
+    float secondAttackTime = 0;
     void Start()
     {
         attackInp = GetComponent<PlayerInput>();
@@ -19,17 +21,40 @@ public class PlayerAttack : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        
         if (Time.time > nextAttackTime)
         {
             isHitting = false;
         }
         animator.SetBool("Attacking", isHitting);
+        
+        if (!(firstAttackTime == 0) && !(secondAttackTime == 0))
+        {
+            animator.SetFloat("Combo", secondAttackTime - firstAttackTime);
+            firstAttackTime = Time.time;
+            secondAttackTime = 0;
+        }
+        if(!(firstAttackTime == 0)&&(Time.time - firstAttackTime >= 1))
+        {
+            firstAttackTime = 0;
+            animator.SetFloat("Combo", 2);
+        }
     }
     void Attack()
     {
+        
         if (CanAttack())
         {
+            if ((firstAttackTime == 0))
+            {
+                firstAttackTime = Time.time;
+            }
+            else if(!(firstAttackTime == 0))
+            {
+                secondAttackTime = Time.time;
+            }
             isHitting = true;
+            //animator.SetBool("isSecond", true);
         }
     }
     bool CanAttack() {
