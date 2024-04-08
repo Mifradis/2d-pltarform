18977c50bbb9a1d555637e1a95c3046e202987c9
bedirhan çakır <bedirhancakir@windowslive.com>
2024-04-08 14:50:27 +0300

    background music added

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class AudioManager : MonoBehaviour
+{
+    [Header (" Audio Source")]
+    [SerializeField] AudioSource musicSource;
+    [SerializeField] AudioSource sfxSource;
+
+    [Header(" Audio Clip")]
+    public AudioClip[] background;
+    public AudioClip hit;
+    public AudioClip death;
+    public AudioClip jump;
+    public AudioClip run;
+    public AudioClip takeHit;
+    public AudioClip attack;
+
+
+    private void Start()
+    {
+        musicSource.clip = background[Random.Range(0,3)];
+        musicSource.Play();
+    }
+
+}
