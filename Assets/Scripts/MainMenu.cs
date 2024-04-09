59c3532d769e5f0be293192cf154fe03f16aa4bc
bedirhan çakır <bedirhancakir@windowslive.com>
2024-04-09 12:25:43 +0300

    basic ui added

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,18 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void GoToScene(string sceneName)
     {
-        
+        SceneManager.LoadScene(sceneName);
+        Debug.Log("Starting");
     }
-
-    // Update is called once per frame
-    void Update()
+    public void Quit()
     {
-        
+        Application.Quit();
+        Debug.Log("Quiting");
     }
 }
